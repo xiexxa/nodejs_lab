6a3c35628a4808c4fdda15be50e5601b54987720
eisuke <eisuk4@gmail.com>
2020-11-03 07:59:05 +0000

    常にカードリーダーを待機するように変更｡当てられるカードによって分岐｡

--- felica/load_idm.py
+++ felica/load_idm.py
@@ -11,22 +11,30 @@ if __name__ == '__main__':
     libpafe = cdll.LoadLibrary("/usr/local/lib/libpafe.so")
 
     libpafe.pasori_open.restype = c_void_p
-    pasori = libpafe.pasori_open()
+    pasori = libpafe.pasori_open() # デバイスファイルのオープン
 
-    libpafe.pasori_init(pasori)
+    libpafe.pasori_init(pasori) # パソリの初期化
 
-    libpafe.felica_polling.restype = c_void_p
-    felica = libpafe.felica_polling(pasori, FELICA_POLLING_ANY, 0, 0)
+    while True:
+        libpafe.felica_polling.restype = c_void_p
+        felica = libpafe.felica_polling(pasori, FELICA_POLLING_ANY, 0, 0) # リクエストコマンドを送信し､以降の通信で必要なデータを取得する
 
-    idm = c_ulonglong() #←16桁受けとるために変更
-    libpafe.felica_get_idm.restype = c_void_p
-    libpafe.felica_get_idm(felica, byref(idm))
+        idm = c_ulonglong() #←16桁受けとるために変更
+        libpafe.felica_get_idm.restype = c_void_p
 
-    # IDmは16進表記
-    print("%016X" % idm.value) #←16桁表示させるために変更
+        libpafe.felica_get_idm(felica, byref(idm)) # 製造識別子の取得｡第2引数にIDmが格納される｡
+
+        # IDmは16進表記
+        print("%016X" % idm.value) #←16桁表示させるために変更
+        if idm.value == int('IDm', 16):
+            print('Suica')
+        elif idm.value == int('IDm', 16):
+            print('三井住友iD')
+        else:
+            print('else')
 
     # READMEより、felica_polling()使用後はfree()を使う
     # なお、freeは自動的にライブラリに入っているもよう
     libpafe.free(felica)
 
-    libpafe.pasori_close(pasori)
+    libpafe.pasori_close(pasori) # デバイスファイルをクローズ
