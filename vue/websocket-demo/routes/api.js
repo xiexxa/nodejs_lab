5c7a151934c87c4b1b502b7467fd0f2d46565a36
eisuke <eisuk4@gmail.com>
2020-11-20 18:38:05 +0900

    setup

---
+++ vue/websocket-demo/routes/api.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const app = require(__dirname+'/../server.js');
+const api_controller = require('../controllers/apiController.js');
+
+module.exports = router;
---
+++ vue/websocket-demo/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const apiRouter = require('./routes/api');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/static/img', express.static(__dirname + '/dist/static/img/'));
+app.use('/static/css', express.static(__dirname + '/dist/static/css/'));
+app.use('/static/js', express.static(__dirname + '/dist/static/js/'));
+
+app.listen(3000, () => console.log('3000'));
+
+app.use('/api', apiRouter);
+
+app.use('/', function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
